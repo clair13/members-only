eef3189d373c7a94b007e892b9ea67eb5c08940f
clair13 <theclairvoyant1313@yahoo.gr>
2019-08-06 15:25:10 +0300

    Create Users model and add validations

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true, length: { maximum: 70 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 300 },format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
+  validates :password, presence: true, length: { maximum: 8 }
+end
